4d69bd0c9147166b15c5a7d29a456985a5ec4c44
Kevin Zhou <c.zhou.ca@gmail.com>
2026-03-26 17:08:41 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
+import { Toaster } from "@/components/ui/toaster"
 import './globals.css'
+import { ErrorReporter, ReactErrorBoundary } from "@/components/error-reporter";
 
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
@@ -37,8 +39,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="font-sans antialiased">
-        {children}
+        <ReactErrorBoundary>
+          {children}
+        </ReactErrorBoundary>
+        <Toaster />
         <Analytics />
+        <ErrorReporter />
       </body>
     </html>
   )
